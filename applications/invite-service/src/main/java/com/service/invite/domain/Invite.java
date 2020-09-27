fbafa12e3d71afadb4965d23c2db8c3157e835b8
Lucas Rohr <lucas.carreno@cwi.com.br>
2020-09-27 19:25:20 -0300

    Fix

--- applications/invite-service/src/main/java/com/service/invite/domain/Invite.java
+++ applications/invite-service/src/main/java/com/service/invite/domain/Invite.java
@@ -1,6 +1,6 @@
 package com.service.invite.domain.entity;
 
-import com.service.common.domain.Account;
+import com.service.common.domain.Heir;
 import com.service.common.domain.Owner;
 import com.service.invite.enums.InviteStatus;
 import lombok.Data;
@@ -18,17 +18,14 @@ public class Invite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Column(nullable = false)
     private InviteStatus status;
-
 
     @ManyToOne
     @JoinColumn(nullable = false, name = "owner_id")
     private Owner owner;
 
-
     @ManyToOne
     @JoinColumn(nullable = false, name = "receiver_id")
-    private Account receiver;
+    private Heir receiver;
 }
