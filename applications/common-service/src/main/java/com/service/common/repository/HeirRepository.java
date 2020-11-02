6bbcb09654a2a2989a315194bf4a56db962c9c8f
Douglas Eduardo Oliveira da Rosa <douglas.rosa@Consulting-for.edenred.com>
2020-11-02 11:09:37 -0300

    wip

--- applications/common-service/src/main/java/com/service/common/repository/HeirRepository.java
+++ applications/common-service/src/main/java/com/service/common/repository/HeirRepository.java
@@ -1,7 +1,12 @@
 package com.service.common.repository;
 
 import com.service.common.domain.Heir;
+import com.service.common.domain.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HeirRepository extends JpaRepository<Heir, Long> {
+
+    public List<Heir> getHeirByOwner(Owner owner);
 }
--- applications/user-service/src/main/java/com/service/user/service/account/GetHeirsByOwner.java
+++
@@ -1,4 +0,0 @@
-package com.service.user.service.account;
-
-public class GetHeirsByOwner {
-}
