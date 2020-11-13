4b8ebda6f2ecfa0a6fcbfe0122803e83c5f56a6e
Lucas Rohr <lucas.carreno@cwi.com.br>
2020-11-12 21:15:28 -0300

    WIP

--- applications/credential-service/src/main/java/com/service/credential/controllers/CredentialController.java
+++ applications/credential-service/src/main/java/com/service/credential/controllers/CredentialController.java
@@ -1,14 +1,18 @@
 package com.service.credential.controllers;
 
+import com.service.common.domain.fabric.credential.CredentialAsset;
 import com.service.credential.controllers.request.CredentialCreationRequest;
 import com.service.credential.services.CreateCredentialService;
+import com.service.credential.services.GetOwnerCredentialsService;
 import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
 import org.hyperledger.fabric.sdk.exception.ProposalException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 
 @RestController
@@ -18,17 +22,21 @@ public class CredentialController {
     @Autowired
     private CreateCredentialService createCredentialService;
 
+    @Autowired
+    private GetOwnerCredentialsService getOwnerCredentialsService;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("creation")
-    public void createCredential(@RequestBody CredentialCreationRequest credentialCreationRequest)
+    public void createCredential(@RequestBody @Validated CredentialCreationRequest credentialCreationRequest)
             throws ProposalException, IOException, InvalidArgumentException {
         createCredentialService.createCredential(credentialCreationRequest);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("owner-credentials")
-    public void getOwnerCredential(@RequestParam("owner_id") Long ownerId) {
-//        createCredentialService.createCredential(credentialCreationRequest);
+    public List<CredentialAsset> getOwnerCredential(@RequestParam("owner_id") Long ownerId)
+            throws ProposalException, IOException, InvalidArgumentException {
+        return getOwnerCredentialsService.getOwnerCredentials(ownerId);
     }
 
 }
