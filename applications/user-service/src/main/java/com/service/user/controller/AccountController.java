f75072fff2110187d9c4861cfd63845fb17244b6
Douglas Eduardo Oliveira da Rosa <oliveira.douglasedu@outlook.com>
2021-01-21 22:25:16 -0300

    basic endpoint

--- applications/user-service/src/main/java/com/service/user/controller/AccountController.java
+++ applications/user-service/src/main/java/com/service/user/controller/AccountController.java
@@ -49,6 +49,12 @@ public class AccountController {
     @Autowired
     private GetCheckAssetsByHeirService getCheckAssetsByHeirService;
 
+    @Autowired
+    private UpdateHeirHeritagesService updateHeirHeritagesService;
+
+    @Autowired
+    private ValidateDeathCertificateService validateDeathCertificateService;
+
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("last-update")
     public void updateLastAccess(@RequestParam("account_id") Long accountId) {
@@ -118,6 +124,14 @@ public class AccountController {
             @RequestBody UpdateHeirHeritageRequest request
     ) {
         updateHeirHeritagesService.updateHeirHeritages(heirId, request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("owner/validate_certificate")
+    public void validateDeathCertificate(
+            @RequestParam("certificate_hash_code") String certificateHashCode
+    ) {
+        validateDeathCertificateService.validateDeathCertificate(certificateHashCode);
     }
 
 }
