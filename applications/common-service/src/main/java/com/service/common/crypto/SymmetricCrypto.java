c02011549bbd5848ae81e2660fd61d98e2679912
Douglas Eduardo Oliveira da Rosa <oliveira.douglasedu@outlook.com>
2021-03-27 17:13:16 -0300

    asymmetric crypto helper

---
+++ applications/common-service/src/main/java/com/service/common/crypto/SymmetricCrypto.java
@@ -0,0 +1,7 @@
+package com.service.common.crypto;
+
+public class SymmetricCrypto {
+    // static function generateKey(password)
+    // static function encrypt()
+    // static function decrypt()
+}
