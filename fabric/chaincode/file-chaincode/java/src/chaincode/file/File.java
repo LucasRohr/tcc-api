1655fc9ee9f3b5c09f32bc33ff2b717e39ad0f5b
Douglas Eduardo Oliveira da Rosa <oliveira.douglasedu@outlook.com>
2021-04-01 23:40:31 -0300

    file chaincode wip

---
+++ fabric/chaincode/file-chaincode/java/src/chaincode/file/File.java
@@ -0,0 +1,18 @@
+package chaincode.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+class File implements Serializable {
+    private Long fileId;
+
+    private String symmetricKey;
+}
